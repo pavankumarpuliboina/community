17801cef04bdc8d2314084ffe831a34eefbe2a4b
nagendrasai363 <nagendrasai363@gmail.com>
2020-10-20 11:21:07 +0530

    Updated Question Detail Page

--- communityapp/context_processors.py
+++ communityapp/context_processors.py
@@ -2,6 +2,6 @@ from communityapp.models import Category,Question,Answer
 from communityapp.forms import QuestionForm
 
 def basefile(request):
-	categorys = Category.objects.all()
+	category = Category.objects.all()
 	form = QuestionForm(request.POST)
-	return {'categorys':categorys, 'form':form}
+	return {'category':category, 'form':form}
--- communityapp/views.py
+++ communityapp/views.py
@@ -18,7 +18,7 @@ def index(request):
 	#separatecategory = Category.objects.filter(slug = slug)
 
 	# question_list = categorys.question_set.all()
-	paginator = Paginator(question, 5)
+	paginator = Paginator(question, 1)
 	page_number = request.GET.get('page')
 	page_obj = paginator.get_page(page_number)
 
@@ -47,7 +47,10 @@ def categorydetail(request,slug):
 def questiondetail(request,slug):
 	question = Question.objects.get(slug = slug)
 	answers = Answer.objects.filter(question = question)
+	recent = [i.question for i in Category.objects.get(category = question.category).question_set.all()][::-1][:5]
 	q = get_object_or_404(Question,slug = slug)
+	related_questions = Question.objects.all()[::-1]
+
 	# answerform = AnswerForm(request.POST)
 	# if request.method == 'POST':
 	# 	if answerform.is_valid():
@@ -58,7 +61,7 @@ def questiondetail(request,slug):
 	# 		return redirect('communityapp:question-detail',slug = q.slug)
 	# 	else:
 	# 		answerform = AnswerForm()
-	return render(request, 'community.html', {'question':question, 'answers':answers})
+	return render(request, 'community.html', {'question':question, 'answers':answers, 'recent':recent, 'related_questions':related_questions})
 
 
 
@@ -96,54 +99,3 @@ def downvote(request):
 		data = {'dov': dov.downvote}
 		print(data)
 	return JsonResponse(data)
-
-
-
-		
-
-
-
-
-
-
-# def questiondetail(request,slug):
-# 	question = Question.objects.get(slug = slug)
-# 	answers = Answer.objects.filter(question = question)
-# 	q = get_object_or_404(Question,slug = slug)
-
-
-
-# 	form = AnswerForm(request.POST)
-# 	if request.method == 'POST':
-# 		#answerdata = AnswerForm(request.POST)
-# 		if form.is_valid():
-# 			answer = form.cleaned_data['answer']
-# 			answer = form.save(commit = False)
-# 			answer.question = q
-# 			answer.user = request.user
-# 			answer.save()
-# 			return redirect('communityapp:question-detail',slug = q.slug)
-# 		else:
-# 			form = AnswerForm()
-
-
-# 	commented_answer = Answer.objects.get(slug = slug)
-# 	comments = Answer.objects.filter(comment = comment)
-# 	s = get_object_or_404(Answer,slug = slug)
-
-# 	commentform = CommentForm(request.POST)
-# 	if request.method == 'POST':
-# 		if commentform.is_valid():
-# 			comment = form.cleaned_data['comment']
-# 			comment =commentform.save(commit = False)
-# 			comments.answer = s
-# 			comment.user = request.user
-# 			comment.save()
-# 			return redirect('communityapp:question-detail',slug = s.slug)
-# 		else:
-# 			commentform = CommentForm
-
-
-
-# 	return render(request, 'community.html', {'question':question,'form':form, 'answers':answers, 'commentform':commentform})
-
